2bc5f315ceb7512b5805a3b6166c5ee7f16b5e21
Aaron Groch <ar.groch@gmail.com>
2016-01-20 08:43:32 -0500

    last night's homework

---
+++ homework-day1/divisible.rb
@@ -0,0 +1,12 @@
+puts "Give me two numbers..."
+print "A larger number: "
+x = gets.chomp.to_i
+print "And a smaller number: "
+y = gets.chomp.to_i
+
+if x % y == 0
+	puts "#{x} is divisible by #{y}! What a lovely day!"
+	puts "The quotient is: #{x/y}"
+else
+	puts "NOT DIVISIBLE! UNCLEAN!"
+end
